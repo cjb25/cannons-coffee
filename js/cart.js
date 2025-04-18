a1d8abf8ed8bf14a1613ef702a1c35484f29ec3d
cjb25 <cjandersondetroit@gmail.com>
2025-04-18 16:28:52 -0400

    updated js

--- js/cart.js
+++ js/cart.js
@@ -14,13 +14,10 @@ function onSubmit(e) {
 
   let name = dataObj.name;
   let userEmail = dataObj.userEmail;
-  let telNo = dataObj.telNo;
-  let comments = dataObj.comments;
-  let coffee = dataObj.coffee;
-  let option = dataObj.option;
+
   let password = dataObj.password;
 
-  let message = `Hi ${name}! We recieved your request ${option};, and wanted to thank you for reaching out. We will be calling you at ${telNo} within the next 24 hrs. Please be sure to check your ${userEmail} inbox for a ${coffee} on us! We appreciate your deatalied coment and look forward to discussing ${comments} with you soon. - The Cannons Coffee Team`;
+  let message = `Welcome ${name}! You are logged in! `;
 
   let messagePara = document.createElement("p");
   messagePara.textContent = message;
